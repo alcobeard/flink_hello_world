15a9857118795aa94883a6281c878683863ec4aa
Aleksandr Bagdasarov <aabagdasarov@sberbank.ru>
2023-02-09 15:23:11 +0300

    init

---
+++ src/main/scala/FraudDetectionJob.scala
@@ -0,0 +1,9 @@
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.walkthrough.common.sink.AlertSink
+import org.apache.flink.walkthrough.common.entity.Alert
+import org.apache.flink.walkthrough.common.entity.Transaction
+import org.apache.flink.walkthrough.common.source.TransactionSource
+
+object FraudDetectionJob {
+
+}
